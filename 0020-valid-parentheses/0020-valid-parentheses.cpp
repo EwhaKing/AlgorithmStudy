121dc992752889462ecf4e26a49039db8ac5e452
Sojin <yunsojiin04@ewhain.net>
2026-02-10 01:47:57 +0900

    0020-valid-parentheses (Easy): Time: 0 ms (100%), Space: 8.9 MB (66.54%)

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -4,9 +4,12 @@ public:
     bool isValid(string s) {
         stack<char> st;
         for(int i=0;i<s.length();i++){
+            //반복문 대신 for(char ch:s)
             if(s[i]=='{' || s[i]=='(' || s[i]=='['){
                 st.push(s[i]);
             }
+            //단점: push하고 나서도 switch문 기웃거림
+            //해결: if, else 로 열린, 닫힌 괄호 명확히 구분
             switch(s[i]){
                 case '}':
                 if(st.empty() || st.top()!='{') return false;
@@ -22,11 +25,15 @@ public:
                 if(st.empty() || st.top()!='[') return false;
                 else st.pop();
                 break;
-
+            
+            //st.empty()확인과 pop이 반복되니까 묶자:
+            //닫힌 괄호일 때 st.empty()->false, 그 뒤로 st.pop();
             }
         }
         if(st.empty())return true;
         else return false;
+
+        //== return st.empty();
     }
 
 };
